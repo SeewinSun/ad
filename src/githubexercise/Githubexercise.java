17622f718f68e853b1fd38aee1043e00605782dd
SeewinSun <59707800+SeewinSun@users.noreply.github.com>
2020-01-23 15:02:44 -0500

    Update Githubexercise.java
    From Github

--- src/githubexercise/Githubexercise.java
+++ src/githubexercise/Githubexercise.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package githubexercise;
 
-/**
- *
- * @author Seewin
- */
-public class Githubexercise {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Githubexercise 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Git Practice");
+        System.out.println("Modified from Github");
     }
-    
 }
