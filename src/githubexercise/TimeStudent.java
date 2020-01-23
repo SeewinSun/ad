5ba441f1c9fbfd0062b0ced14f5fb9eee486626f
SeewinSun <seewinsun@gmail.com>
2020-01-23 15:24:39 -0500

    COMMIT

---
+++ src/githubexercise/TimeStudent.java
@@ -0,0 +1,14 @@
+package githubexercise;
+
+/**
+ * @author Seewin Sun
+ */
+//public class TimeStudent extends Student
+public class TimeStudent 
+{
+    public TimeStudent()
+    {
+        
+    }
+    
+}
